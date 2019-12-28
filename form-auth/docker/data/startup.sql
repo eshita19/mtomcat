ea1d5bc653d08c0e0489a3fb935138a8e50f4506
emathur <emathur@cisco.com>
2019-12-28 17:12:03 +0530

    Changes

---
+++ form-auth/docker/data/startup.sql
@@ -0,0 +1,17 @@
+create schema db1;
+use db1;
+create table users (
+  user_name         varchar(15) not null primary key,
+  user_pass         varchar(15) not null
+);
+
+create table user_roles (
+  user_name         varchar(15) not null,
+  role_name         varchar(15) not null,
+  primary key (user_name, role_name)
+);
+
+insert into users values('eshita', 'eshita');
+insert into user_roles values('eshita', 'admin');	
+
+
